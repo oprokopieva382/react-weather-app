7c790ef18da6a848c538a4c4e4052ff4e645d0be
oprokopieva382 <oprokopieva382@gmail.com>
2022-08-05 09:27:19 -0500

    added github url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weater App</h1>
-      <footer></footer>
+      <footer>
+        This site was coded by Ksy Fate and is{" "}
+        <a
+          href="https://github.com/oprokopieva382/react-weather-app"
+          target="_blank"
+        >
+          open source
+        </a>
+      </footer>
     </div>
   );
 }
