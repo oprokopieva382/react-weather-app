a803fc1faf79148b9dd2566e4c1666c0598bb9cb
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-07-01 02:44:37 -0500

    fixed minutes + hours

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -12,12 +12,14 @@ export default function FormattedDate(props) {
   ];
   let day = days[props.date.getDay()];
   let minutes = props.date.getMinutes();
+  console.log(minutes)
   if (minutes < 10) {
-    minutes = `0{minutes}`;
+    minutes = `0${minutes}`;
   }
   let hours = props.date.getHours();
+   console.log(hours);
   if (hours < 10) {
-    hours = `0{hours}`;
+    hours = `0${hours}`
   }
   return (
     <div>
